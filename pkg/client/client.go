eca03cbd71fabb3a5e8c5975acae11fd5383cf7a
wencaiwulue <895703375@qq.com>
2021-03-09 22:54:42 +0800

    add etcd client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -6,6 +6,9 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	kubeadmapiv1beta1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta1"
+	configutil "k8s.io/kubernetes/cmd/kubeadm/app/util/config"
+	"k8s.io/kubernetes/cmd/kubeadm/app/util/etcd"
 	serving "knative.dev/serving/pkg/client/clientset/versioned"
 	"sync"
 )
@@ -14,9 +17,11 @@ var client *Client
 var lock sync.Mutex
 
 type Client struct {
+	Config        *rest.Config
 	K8sClient     *kubernetes.Clientset
 	ServingClient *serving.Clientset
 	TektonClient  *tektoncd.Clientset
+	EtcdClient    *etcd.Client
 }
 
 func GetClient() *Client {
@@ -50,10 +55,20 @@ func initClient(inCluster bool) {
 	if err != nil {
 		fmt.Printf("error create tekton client: %v", err)
 	}
+	internalcfg, err := configutil.DefaultedInitConfiguration(&kubeadmapiv1beta1.InitConfiguration{})
+	if err != nil {
+		fmt.Printf("unexpected error getting default config: %v", err)
+	}
+	etcdClient, err := etcd.NewFromCluster(k8sClient, internalcfg.CertificatesDir)
+	if etcdClient == nil || err != nil {
+		fmt.Printf("new client with DialNoWait should succeed, got %v", err)
+	}
 
 	client = &Client{
+		Config:        config,
 		K8sClient:     k8sClient,
 		ServingClient: servingClient,
 		TektonClient:  tektonClient,
+		EtcdClient:    etcdClient,
 	}
 }
