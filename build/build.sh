20713bb898fce7e0d83e3e3a41b8f432aefe9deb
p_caiwfeng <p_caiwfeng@tencent.com>
2021-03-21 16:36:54 +0800

    add restart pod script in build.sh

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,18 @@
 
 go env -w GOPROXY=https://goproxy.cn,direct
 go mod download
-CGO_ENABLED=0 GOOS=linux go build -mod=readonly -v -o server knativetest/cmd/"$1"
+CGO_ENABLED=0 GOOS=linux go build -mod=readonly -v -o "$1" knativetest/cmd/"$1"
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 IMAGE=$1:latest
 docker build -t "$IMAGE" "$DIR"/../ -f "$DIR"/../deploy/Dockerfile_"$1"
+rm "$1"
+
+# restart the pods
+for POD in $(kubectl get pods -n test | awk -F ' ' '{print$1}' | grep -v NAME); do
+  # shellcheck disable=SC2046
+  if [ "$IMAGE" == $(kubectl get pods "$POD" -n test -o jsonpath='{.spec.containers[0].image}') ]; then
+    kubectl delete pods "$POD" -n test
+    break
+  fi
+done
--- manifest/knative/restartpod.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# shellcheck disable=SC2046
-kubectl delete pods $(kubectl get pods -n test | grep webhook | awk -F' ' '{print$1}') -n test
